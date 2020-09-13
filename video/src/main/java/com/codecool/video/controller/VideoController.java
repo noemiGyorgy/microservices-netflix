ff49ea8fad67fa0cf96ea0f59bb0aaf4068f56d3
noemi <gynoemi33@gmail.com>
2020-09-13 18:37:30 +0200

    Implement updating video

--- video/src/main/java/com/codecool/video/controller/VideoController.java
+++ video/src/main/java/com/codecool/video/controller/VideoController.java
@@ -22,8 +22,8 @@ public class VideoController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Value("${recommendation.url}")
-    private String baseUrl;
+    //@Value("${recommendation.url}")
+    private String baseUrl = "http://localhost:8090/review";
 
     @GetMapping("/list")
     public List<Video> videoList() {
@@ -34,13 +34,22 @@ public class VideoController {
     public VideoDetails retrieveVideo(@PathVariable("id") String id) {
         Video video = videoRepository.findById(Long.valueOf(id)).orElse(null);
 
-       // Review[] reviews = restTemplate.getForEntity(baseUrl + "/video/" + id.toString(), Review[].class).getBody();
-        Review[] reviews = restTemplate.getForEntity("http://localhost:8090/review/video/" + id, Review[].class).getBody();
+        Review[] reviews = restTemplate.getForEntity(baseUrl + "/video/" + id, Review[].class).getBody();
 
         return new VideoDetails(video, reviews);
     }
 
-    @PostMapping("/add_review")
+    @PostMapping("/updateVideo/{id}")
+    public void updateVideo(@RequestBody Video newVideo) {
+        Video video = videoRepository.findById(newVideo.getId()).orElse(null);
+        if (video != null) {
+            video.setName(newVideo.getName());
+            video.setUrl(newVideo.getUrl());
+            videoRepository.save(video);
+        }
+    }
+
+    @PostMapping("/addReview")
     public void addReview(@RequestBody Review review) {
         HttpEntity<Review> request = new HttpEntity<>(review);
         restTemplate.postForObject(baseUrl + "/add", request, Review.class);
